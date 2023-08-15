a1afb7e7e42618973ff76b0f97bc8668a9338bdd
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-15 16:04:08 -0300

    initial commit

---
+++ tipos-variaveis/src/Tipos.java
@@ -0,0 +1,17 @@
+public class Tipos {
+    public static void main (String[]args){
+        //existem 8 tipos primitivos em java, sendo eles : int, byte
+        //short,long, float,double , boolean  e char. Isso quer dizer que esses
+        // tipos não são considerados objetos, representam valores brutos. são armazenados diretamente na pilha de memoria.
+
+        // tipo > nome da variavel > valor da variavel
+        int idade;
+        double salario = 2500;
+        long cpf = 4276897263L;
+        float pi = 3.14f;
+
+
+
+    }
+
+}
