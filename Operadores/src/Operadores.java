532e2f2896c2d38be9b896673371d1f821a9a300
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-17 19:13:49 -0300

    fourth commit

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -35,8 +35,58 @@ public class Operadores {
 
         int numero = 5;
         //numero = -numero; se eu quiser tornar o numero negativo eu faço isso.
-        System.out.println(- numero);
+        //System.out.println(- numero);
         //numero = numero * -1 ; assim transformo do negativo para o positivo.
+        //numero = numero + 2;//incrementando em + 2
+        numero--;
+        System.out.println(numero);
+
+        boolean variavel = true;
+        variavel = !variavel;//inverti o valor lógico de true para false
+        System.out.println(variavel);
+
+        //operador ternario, é uma forma resumida para definir uma condição e escolher por um
+        //dentre dois valores. é representado pelos símbolos ?:
+        int a,b;
+        a = 6;
+        b = 6;
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+        //operadores relacionais avaliam a relação entre duas variáveis ou expressões;
+        //utilizando os simbolos:;
+        // == para identificar se uma variavel é igual a outra;
+        // != para identificar se uma variavel é diferente da outra;
+        // > para identificar se uma variavel é maior que a outra;
+        // >= para identificar se uma variavel é maior ou igual a outra;
+        // < para identificar se uma variavel é menor que a outra;
+        // <= para identificar se uma variavel é menor ou igual a outra;
+
+        int numeroUm = 3;
+        int numeroDois = 4;
+
+        boolean simNao = numeroUm > numeroDois;
+        System.out.println("o numeroUm é maior do que o numeroDois? " + simNao);
+
+        simNao = numeroUm < numeroDois;
+        System.out.println("o numeroUm é menor do que o numeroDois? " + simNao);
+
+        simNao = numeroUm != numeroDois;
+        System.out.println("o numeroUm é diferente do numeroDois? " + simNao);
+
+        //operadores lógicos, representam o recurso que nos permite criar expressçoes lógicas maiores;
+        //%% significa "e"
+        //|| significa "Ou"
+
+        boolean condição1 = true;
+        boolean condição2 = false;
+        if (condição1 && condição2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if (condição1 || condição2 ){
+            System.out.println("umas das duas é verdadeira");
+        }
+        System.out.println("acabou.");
 
 
 
