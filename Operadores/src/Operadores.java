02ae7f55fb28215ed111dcf1a4826c7760bd0cb7
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-17 14:43:36 -0300

    operators commit

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main (String[] args){
+
+        //operador de atribuição, usa o simbolo =
+        // é utilizado para definir o valor inicial ou sobrescrever
+        //o valor de uma variável.
+        //exemplo
+        int idade = 22;
+        String nome = "Juliano";
+        double peso = 78;
+
+        //operador aritimético, é utilizado para realizar operações
+        //matemáticas entre valores numericos.
+        // + , - , * , /
+        double soma = 10 + 5;
+        int subtração = 100 - 50;
+        int multiplicação = 20 * 7;
+        int divisão = 15 / 3;
+        int modulo = 18 % 3; // descubro o resto de uma divisão
+
+
+        // com "+" consigo concatenizar textos.
+
+        String nomeCompleto = "juliano" + " lopes";
+        System.out.println(nomeCompleto);
+
+
+
+
+    }
+}
