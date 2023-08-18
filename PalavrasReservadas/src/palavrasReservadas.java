b60e8b1cf44402af42dfd78e28a52396d92df01e
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-17 22:28:50 -0300

    palavras reservadas commit 6

---
+++ PalavrasReservadas/src/palavrasReservadas.java
@@ -0,0 +1,7 @@
+public class palavrasReservadas {
+//palavras reservadas são identificadores de uma linguagem que ja possuem uma finalidade específica
+//portanto não poderia ser utilizada para nomear variaveis, classes, metodos ou atributos, por exemplo, nao posso nomear
+// uma variavel como int. int int = 1; por exemplo não tem como fazer.
+// modificadores de acesso são palavras reservadas.
+// sendo elas public, private e protected.
+}
