5c9c8dc5042dbfd07b2749ff392d56b116d922dc
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-30 20:58:21 -0300

    Nomenclatura poo

---
+++ ProgramacaoOrientadaAObjetos1/src/com/microsoft/model/Cliente1.java
@@ -0,0 +1,5 @@
+package com.microsoft.model;
+
+public class Cliente1 {
+    //aqui conterá a camada e o modelo da aplicação, clientes, usuarios, pedidos, notas fiscais etc
+}
---
+++ ProgramacaoOrientadaAObjetos1/src/com/microsoft/service/ImprimirNotaFiscal.java
@@ -0,0 +1,5 @@
+package com.microsoft.service;
+
+public class ImprimirNotaFiscal {
+    //aqui é o pacote de service, aqui conterá toda a regra de negocio, como validação de cpf, e validação de pedidos.
+}
