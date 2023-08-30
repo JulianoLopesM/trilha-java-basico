5dcd2dec02b34af630c756c4c1fd0f8c12cfcd0f
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-30 20:42:50 -0300

    Nomenclatura dos pacotes

--- ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Cliente.java
+++
@@ -1,5 +0,0 @@
-package com.powersoft.model;
-
-public class Cliente {
-//model é onde terá o modelo de aplicação, isso é: clientes, nota fiscais,pedidos, usuarios.
-}
---
+++ ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Usuario.java
@@ -0,0 +1,7 @@
+package com.powersoft.model;
+
+public class Usuario {
+
+        //por ser do tipo model, conterá os clientes, pedidos, notais fiscais, usuarios etc;
+
+}
