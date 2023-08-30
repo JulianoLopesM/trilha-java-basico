d104d89b5164fb95ae420f78a07b198bde1f6f4f
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-30 20:37:30 -0300

    Nomenclatura dos pacotes

---
+++ ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.powersoft.model;
+
+public class Cliente {
+//model é onde terá o modelo de aplicação, isso é: clientes, nota fiscais,pedidos, usuarios.
+}
---
+++ ProgramacaoOrientadaAObjetos/src/com/powersoft/service/CalcularIpva.java
@@ -0,0 +1,5 @@
+package com.powersoft.service;
+
+public class CalcularIpva {
+    //é o package onde terá as regras dos negócios, por exemplo validar cpfs, fazer contas, e afins.
+}
