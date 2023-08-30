8c3cf06b92da2727a25b0c6d4f651f42f919a671
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-30 20:52:01 -0300

    Nomenclatura dos pacotes

---
+++ ProgramacaoOrientadaAObjetos/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Vamos imaginar, que sua empresa se chama Power Soft e ela está desenvolvendo software comercial, governamental
+        //e um software livre ou de código aberto. Abaixo teríamos os pacotes sugeridos conforme tabela abaixo:
+        //Comercial : com.powersoft;
+        //Governamental : gov.powersoft;
+        //Código aberto: org.powersoft.
+        //model : Classes que representam a camada e modelo da aplicação : Cliente, Pedido, NotaFiscal, Usuario;
+        //repository: Classes ou interfaces que possuem a finalidade de interagir com tabelas no banco de dados: ClienteRepository;
+        //service: Classes que contém regras de negócio do sistema : ClienteService possui o método validar o CPF, do cliente cadastrado;
+        //controller: Classes que possuem a finalidade de, disponibilizar os nossos recursos da aplicação, para outras aplicações via padrão HTTP;
+        //view: Classes que possuem alguma interação, com a interface gráfica acessada pelo usuário;
+        //util: Pacote que contém, classes utilitárias do sistema: FormatadorNumeroUtil, ValidadorUtil.
+
+
+    }
+}
---
+++ ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.powersoft.model;
+
+public class Cliente {
+    //aqui é o package model, aqui conterá a camada de aplicação, ou seja, clientes, usuarios, notas fiscais,pedidos e etc.
+}
