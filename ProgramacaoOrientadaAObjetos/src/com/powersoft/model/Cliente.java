2b291e338262a12e53ff4a1ccb5d2d1970482647
JulianoLopesM <135735980+JulianoLopesM@users.noreply.github.com>
2023-08-30 22:38:11 -0300

    Delete ProgramacaoOrientadaAObjetos directory

--- ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Cliente.java
+++
@@ -1,5 +0,0 @@
-package com.powersoft.model;
-
-public class Cliente {
-    //aqui é o package model, aqui conterá a camada de aplicação, ou seja, clientes, usuarios, notas fiscais,pedidos e etc.
-}
--- ProgramacaoOrientadaAObjetos/src/com/powersoft/model/Usuario.java
+++
@@ -1,7 +0,0 @@
-package com.powersoft.model;
-
-public class Usuario {
-
-        //por ser do tipo model, conterá os clientes, pedidos, notais fiscais, usuarios etc;
-
-}
